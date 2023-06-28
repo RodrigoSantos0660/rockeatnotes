c8f2d1989da35bab628d44d2bfb176646b23c71d
RodrigoSantos <passinhorcs@hotmail.com>
2023-06-28 14:51:43 -0300

    two class

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './style/global';
 
 import theme from './style/theme';
 
@@ -10,6 +11,7 @@ import { Details } from './pages/Details'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}> 
+      <GlobalStyles/>
       <Details />
     </ThemeProvider>
   </React.StrictMode>,
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,12 +1,20 @@
 import { Container } from './styles';
 
+import { Button } from '../../components/button';
+
 export function Details() {
 
   return(
     <Container>
       <h1>hello world</h1>
+
+      < Button  title="login" loading />
+      < Button title="register" />
+      < Button  title="back" />
+   
+
     </Container>
 
-   )
+   );
 }
 
